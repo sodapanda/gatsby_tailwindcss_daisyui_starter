0b3f64fb453b1c9e9c365980965a9104b2eca713
leucas <sodapanda20@gmail.com>
2023-02-02 16:46:59 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `product_idea_finder`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
