42fa04f2a149aa0e7159482ddd745618fa3de0e9
leucas <sodapanda20@gmail.com>
2023-02-03 13:32:16 +0800

    [add]init starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `product_idea_finder`,
+    title: `gatsby_tailwind_daisyui`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: ["gatsby-plugin-postcss"],
